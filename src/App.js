75b4176084895d96d10fb2a0c45db6385d3943b5
ephieo <ephieo@rocketmail.com>
2025-04-16 11:11:11 +0100

    adding basic setup and resources for link page

--- src/App.js
+++ src/App.js
@@ -1,25 +1,40 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="flex h-screen flex-col items-center justify-center  text-sky-950 bg-lime-200 cursor-pointer ">
+        <h1 className="text-bold text-5xl">Ephie Oyedoh</h1>
+        <div className="flex flex-col">
+          {socialsResources.map((resource) => (
+            <a
+              className="p-4 m-8  font-semibold hover:text-red-400 hover:text-2xl hover:border-dotted hover:border-4 border-red-400 rounded-xl "
+              href={resource.href}
+              key={resource.name}
+              target="_blank"
+            >
+              {resource.name}
+            </a>
+          ))}
+        </div>
+      </div>
     </div>
   );
 }
 
 export default App;
+
+const socialsResources = [
+  {
+    name: "Instagram",
+    logo: "",
+    href: "https://www.instagram.com/phratzphotoz",
+  },
+  { name: "Youtube", logo: "", href: "https://www.youtube.com/@EphiesWorld" },
+  { name: "Tiktok", logo: "", href: "https://www.tiktok.com/@eshego" },
+  {
+    name: "Photography Portfolio",
+    logo: "",
+    href: "https://www.koinonia835.com/",
+  },
+];
